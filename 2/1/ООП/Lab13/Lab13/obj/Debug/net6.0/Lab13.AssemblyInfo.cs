2963d5bf826a753aa74c3517da4efb1571099137
Litvinas <vaclovkacinskas@gmail.com>
2025-01-04 11:38:07 +0300

    last

--- 2/1/ООП/Lab13/Lab13/obj/Debug/net6.0/Lab13.AssemblyInfo.cs
+++ 2/1/ООП/Lab13/Lab13/obj/Debug/net6.0/Lab13.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d9809a46be3013a453b412960908c58ce4ec4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a77aac9d8280a19efa4e294c8e402f4b1e9a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
