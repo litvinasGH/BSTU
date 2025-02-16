c2f4691ac1e9255922a9f0408b2329e175951b3c
Litvinas <vaclovkacinskas@gmail.com>
2025-02-16 12:13:44 +0300

    first lab oop

--- 2/2/ООП/lab1/lab1/Calculator.cs
+++ 2/2/ООП/lab1/lab1/Calculator.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace lab1
 {
-    internal interface Ections
+    internal interface ICalculator
     {
         int AND_Calc();
         int OR_Calc();
         int XOR_Calc();
         int NO_Calc();
     }
-    
-    internal class Calculator : Ections
+
+    internal class Calculator : ICalculator
     {
         private int first_num;
         private int second_num;
@@ -25,7 +19,7 @@ namespace lab1
         {
             this.first_num = Convert.ToInt32(first_num, num);
             if (!b)
-            this.second_num = Convert.ToInt32(second_num, num);
+                this.second_num = Convert.ToInt32(second_num, num);
         }
 
         public int AND_Calc()
@@ -33,17 +27,17 @@ namespace lab1
             return first_num & second_num;
         }
 
-        public int NO_Calc()
+        public int OR_Calc()
         {
             return first_num | second_num;
         }
 
-        public int OR_Calc()
+        public int XOR_Calc()
         {
             return first_num ^ second_num;
         }
 
-        public int XOR_Calc()
+        public int NO_Calc()
         {
             return ~first_num;
         }
--- 2/2/ООП/lab1/lab1/Form1.cs
+++ 2/2/ООП/lab1/lab1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lab1
@@ -22,9 +15,7 @@ namespace lab1
             try
             {
                 int num = 2;
-                if (bin.Checked)
-                    num = 2;
-                else if (eth.Checked)
+                if (eth.Checked)
                     num = 8;
                 else if (ten.Checked)
                     num = 10;
@@ -32,22 +23,112 @@ namespace lab1
                     num = 16;
                 Calculator calc = new Calculator(textBox1.Text, textBox2.Text, num, radioButton_NO.Checked);
 
-                int result = 0;
+                int result;
                 if (radioButton_AND.Checked)
                     result = calc.AND_Calc();
                 else if (radioButton_OR.Checked)
                     result = calc.OR_Calc();
                 else if (radioButton_XOR.Checked)
                     result = calc.XOR_Calc();
-                else if (radioButton_NO.Checked)
+                else
                     result = calc.NO_Calc();
 
 
-                string result_out = "Бинарный"
+                string result_out = $"Бинарный: {Convert.ToString(result, 2)}\n" +
+                    $"Восмеричный: {Convert.ToString(result, 8)}\n" +
+                    $"Десятичный: {result}\n" +
+                    $"Шестнадцатиричный: {Convert.ToString(result, 16).ToUpper()}";
+
+                MessageBox.Show(result_out, "Результат", MessageBoxButtons.OK);
             }
-            catch (Exception ex) 
+            catch (ArgumentOutOfRangeException ex)
             {
-                MessageBox.Show(ex.ToString(), ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                var err = MessageBox.Show("Имеются пустые строки!",
+                    ex.Message, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                switch (err)
+                {
+                    case DialogResult.Abort:
+                        Application.Exit();
+                        break;
+
+                    case DialogResult.Retry:
+                        button1_Click(this, new EventArgs());
+                        break;
+
+                    case DialogResult.Ignore:
+
+                        break;
+                }
+            }
+            catch (FormatException ex)
+            {
+                var err = MessageBox.Show("Неверный формат чисел!",
+                    ex.Message, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                switch (err)
+                {
+                    case DialogResult.Abort:
+                        Application.Exit();
+                        break;
+
+                    case DialogResult.Retry:
+                        button1_Click(this, new EventArgs());
+                        break;
+
+                    case DialogResult.Ignore:
+
+                        break;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                var err = MessageBox.Show(ex.Message,
+                    ex.Message, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                switch (err)
+                {
+                    case DialogResult.Abort:
+                        Application.Exit();
+                        break;
+
+                    case DialogResult.Retry:
+                        button1_Click(this, new EventArgs());
+                        break;
+
+                    case DialogResult.Ignore:
+
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                var err = MessageBox.Show(ex.ToString(), ex.Message,
+                    MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                switch (err)
+                {
+                    case DialogResult.Abort:
+                        Application.Exit();
+                        break;
+
+                    case DialogResult.Retry:
+                        button1_Click(this, new EventArgs());
+                        break;
+
+                    case DialogResult.Ignore:
+
+                        break;
+                }
+            }
+        }
+
+        private void radioButton_NO_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_NO.Checked)
+            {
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                textBox2.Enabled = true;
             }
         }
     }
