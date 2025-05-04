ecbaadf16d09ed7a0a3ba3bfaaa13b7bd2846933
Litvinas <vaclovkacinskas@gmail.com>
2025-05-04 18:31:21 +0300

    test

---
+++ 2/2/ООП/lab4/Printinvest/ViewModels/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+namespace MyApp.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
