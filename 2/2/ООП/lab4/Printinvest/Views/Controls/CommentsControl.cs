96ce4f2967dc6a1266f0daad0bedffc88e0aa8f5
Litvinas <vaclovkacinskas@gmail.com>
2025-05-11 22:37:26 +0300

    g

---
+++ 2/2/ООП/lab4/Printinvest/Views/Controls/CommentsControl.cs
@@ -0,0 +1,125 @@
+﻿using Printinvest.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControls
+{
+
+    public class CommentsControl : Control
+    {
+        static CommentsControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommentsControl),
+                new FrameworkPropertyMetadata(typeof(CommentsControl)));
+        }
+
+        // Comments collection
+        public static readonly DependencyProperty CommentsProperty =
+            DependencyProperty.Register(nameof(Comments), typeof(ObservableCollection<Comment>),
+                typeof(CommentsControl), new PropertyMetadata(new ObservableCollection<Comment>()));
+        public ObservableCollection<Comment> Comments
+        {
+            get => (ObservableCollection<Comment>)GetValue(CommentsProperty);
+            set => SetValue(CommentsProperty, value);
+        }
+
+        // NewCommentText
+        public static readonly DependencyProperty NewCommentTextProperty =
+            DependencyProperty.Register(nameof(NewCommentText), typeof(string), typeof(CommentsControl),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public string NewCommentText
+        {
+            get => (string)GetValue(NewCommentTextProperty);
+            set => SetValue(NewCommentTextProperty, value);
+        }
+
+        // AddComment command
+        public static readonly RoutedUICommand AddCommentCommand = new RoutedUICommand(
+            "AddComment", "AddComment", typeof(CommentsControl));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            CommandBindings.Add(new CommandBinding(AddCommentCommand, OnAddComment, CanAddComment));
+
+
+            // Привязываем команду к кнопке
+            if (GetTemplateChild("PART_AddButton") is Button btn)
+                btn.Command = AddCommentCommand;
+
+            // Привязываем TextBox
+            if (GetTemplateChild("PART_TextBox") is TextBox tb)
+                tb.SetBinding(TextBox.TextProperty,
+                    new Binding(nameof(NewCommentText)) { Source = this, Mode = BindingMode.TwoWay });
+
+            if (GetTemplateChild("PART_ItemsPresenter") is ItemsPresenter ip)
+            {
+                // nothing
+            }
+        }
+
+        public static readonly DependencyProperty NewCommentRatingProperty =
+    DependencyProperty.Register(nameof(NewCommentRating), typeof(double),
+        typeof(CommentsControl), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double NewCommentRating
+        {
+            get => (double)GetValue(NewCommentRatingProperty);
+            set => SetValue(NewCommentRatingProperty, value);
+        }
+
+
+        public static readonly DependencyProperty CurrentUserProperty =
+        DependencyProperty.Register(
+            nameof(CurrentUser), typeof(User), typeof(CommentsControl),
+            new PropertyMetadata(null, OnCurrentUserChanged));
+
+        public User CurrentUser
+        {
+            get => (User)GetValue(CurrentUserProperty);
+            set => SetValue(CurrentUserProperty, value);
+        }
+
+        private static void OnCurrentUserChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // При смене CurrentUser можно, например, очищать поле ввода:
+            var ctrl = (CommentsControl)d;
+            ctrl.NewCommentText = string.Empty;
+        }
+
+        // 2) В CanAddComment проверяем, что есть CurrentUser
+        private void CanAddComment(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = CurrentUser != null
+                           && !string.IsNullOrWhiteSpace(NewCommentText);
+        }
+
+        // 3) В OnAddComment используем CurrentUser, а не Environment.UserName
+        private void OnAddComment(object sender, ExecutedRoutedEventArgs e)
+        {
+            Comments.Add(new Comment
+            {
+                Author = CurrentUser.Name,
+                Text = NewCommentText.Trim(),
+                Rating = NewCommentRating == -1 ? 0 : NewCommentRating == 6 ? 5 : NewCommentRating,
+                Timestamp = DateTime.Now
+            });
+
+            NewCommentText = string.Empty;
+            NewCommentRating = 0.0;
+        }
+    }
+}
---
+++ 2/2/ООП/lab4/Printinvest/Views/Controls/NumericUpDown.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CustomControls
+{
+    public class NumericUpDown : Control
+    {
+        static NumericUpDown() => DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDown), new FrameworkPropertyMetadata(typeof(NumericUpDown)));
+
+        // Min property
+        public static readonly DependencyProperty MinProperty = DependencyProperty.Register(
+            nameof(Min), typeof(int), typeof(NumericUpDown),
+            new PropertyMetadata(0, OnMinMaxChanged));
+
+        public int Min
+        {
+            get => (int)GetValue(MinProperty);
+            set => SetValue(MinProperty, value);
+        }
+
+        // Max property
+        public static readonly DependencyProperty MaxProperty = DependencyProperty.Register(
+            nameof(Max), typeof(int), typeof(NumericUpDown),
+            new PropertyMetadata(100, OnMinMaxChanged));
+
+        public int Max
+        {
+            get => (int)GetValue(MaxProperty);
+            set => SetValue(MaxProperty, value);
+        }
+
+        private static void OnMinMaxChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (NumericUpDown)d;
+            control.CoerceValue(ValueProperty);
+        }
+
+        // Step property
+        public static readonly DependencyProperty StepProperty = DependencyProperty.Register(
+            nameof(Step), typeof(int), typeof(NumericUpDown),
+            new PropertyMetadata(1));
+
+        public int Step
+        {
+            get => (int)GetValue(StepProperty);
+            set => SetValue(StepProperty, value);
+        }
+
+        // Value property
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), typeof(int), typeof(NumericUpDown),
+            new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnValueChanged, CoerceValue),
+            ValidateValueCallback);
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        private static bool ValidateValueCallback(object v) => v is int;
+
+        private static object CoerceValue(DependencyObject d, object baseValue)
+        {
+            var control = (NumericUpDown)d;
+            var val = (int)baseValue;
+            if (val < control.Min) return control.Min;
+            if (val > control.Max) return control.Max;
+            return val;
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (NumericUpDown)d;
+            control.RaiseValueChangedEvent((int)e.OldValue, (int)e.NewValue);
+        }
+
+        // RoutedEvent: Direct
+        public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(
+            "ValueChanged", RoutingStrategy.Direct, typeof(RoutedPropertyChangedEventHandler<int>), typeof(NumericUpDown));
+
+        public event RoutedPropertyChangedEventHandler<int> ValueChanged
+        {
+            add => AddHandler(ValueChangedEvent, value);
+            remove => RemoveHandler(ValueChangedEvent, value);
+        }
+
+        void RaiseValueChangedEvent(int oldVal, int newVal)
+        {
+            var args = new RoutedPropertyChangedEventArgs<int>(oldVal, newVal) { RoutedEvent = ValueChangedEvent };
+            RaiseEvent(args);
+        }
+
+        // Increment/Decrement commands
+        public static RoutedUICommand IncreaseCommand = new RoutedUICommand("Increase", "Increase", typeof(NumericUpDown));
+        public static RoutedUICommand DecreaseCommand = new RoutedUICommand("Decrease", "Decrease", typeof(NumericUpDown));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (GetTemplateChild("PART_Increase") is Button inc)
+                inc.Command = IncreaseCommand;
+            if (GetTemplateChild("PART_Decrease") is Button dec)
+                dec.Command = DecreaseCommand;
+
+            CommandBindings.Add(new CommandBinding(IncreaseCommand, (s, e) => Value += Step, (s, e) => e.CanExecute = Value + Step <= Max));
+            CommandBindings.Add(new CommandBinding(DecreaseCommand, (s, e) => Value -= Step, (s, e) => e.CanExecute = Value - Step >= Min));
+        }
+    }
+}
