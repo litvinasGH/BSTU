9e9d5be344f755f4ee9c748cb674e9fa3e686b23
Litvinas <vaclovkacinskas@gmail.com>
2025-10-19 18:15:16 +0300

    aspa

--- 3/1/Технология программирования в интернет/1/ASPA/BSTU.Results.Authenticate/IAuthenticate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BSTU.Results.Authenticate
-{
-    public interface IAuthenticate
-    {
-        Task<bool> SignInAsync(string login, string password);
-        Task SignOutAsync();
-    }
-}
